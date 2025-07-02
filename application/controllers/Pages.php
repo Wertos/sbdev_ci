6b95371d52bd03b5865b1c060911846d7aa1533d
Евгений <tutaew@ya.ru>
2025-07-02 21:29:20 +0300

    Update Pages.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -11,29 +11,26 @@
 
     public function rules()
     {
-			$this->breadcrumb->append('Правила');
-
-			$this->template->title = 'Правила';
-            $this->template->content->view('rules', $data);
-            $this->template->publish();
+	$this->breadcrumb->append('Правила');
+	$this->template->title = 'Правила';
+        $this->template->content->view('rules');
+        $this->template->publish();
     }
 
     public function secure()
     {
-			$this->breadcrumb->append('Обход блокировок');
-
-			$this->template->title = 'Обход блокировок';
-            $this->template->content->view('secure', $data);
-            $this->template->publish();
+	$this->breadcrumb->append('Обход блокировок');
+	$this->template->title = 'Обход блокировок';
+	$this->template->content->view('secure');
+        $this->template->publish();
     }
 
     public function what()
     {
-			$this->breadcrumb->append('Как и зачем');
-
-			$this->template->title = 'Как и зачем';
-            $this->template->content->view('what', $data);
-            $this->template->publish();
+	$this->breadcrumb->append('Как и зачем');
+        $this->template->title = 'Как и зачем';
+        $this->template->content->view('what');
+        $this->template->publish();
     }
 
 }
